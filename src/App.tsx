03029c449024a8cba8963a4b52f0e0d71651bfcc
Peter Francois <peter.francois237@hotmail.com>
2025-09-08 16:49:31 +0200

    Install @tanstack/react-query-devtools and use tanstack in main
    Co-authored-by: Bipce <Bipce@users.noreply.github.com>
    Co-authored-by: NicolasPalay <NicolasPalay@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import DisconnectedLayout from "./layouts/DisconnectedLayout";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const App = () => {
   return (
+    <>
     <Routes>
       <Route path="/" element={<DisconnectedLayout />}>
         <Route index element={<Home />} />
       </Route>
     </Routes>
+    < ReactQueryDevtools />
+    </>
   );
 };
 

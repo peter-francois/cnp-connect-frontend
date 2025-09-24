bd64595ea626c2a68f574642df0f2e99929005df
Claire ROYER <claireroyer57@gmail.com>
2025-09-24 13:55:35 +0200

    Edit StatusIsConnect.tsx

--- src/components/user/StatusIsConnect.tsx
+++ src/components/user/StatusIsConnect.tsx
@@ -2,7 +2,7 @@ interface StatusIsConnectInterface {
   status: boolean;
 }
 const StatusIsConnect = ({ status }: StatusIsConnectInterface) => {
-  return <div className={`rounded-full mt-1 ml-2 w-3 h-3 p-2 ${status ? "bg-green-600" : "bg-red-600"}`}></div>;
+  return <div className={`rounded-full mt-1 ml-2 size-3 p-2 ${status ? "bg-green-600" : "bg-red-600"}`}></div>;
 };
 
 export default StatusIsConnect;

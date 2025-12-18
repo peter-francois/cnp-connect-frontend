80bb7bc1452b6f2979534cfe5b058a2e2cc4abe1
Claire Royer <claireroyer57@gmail.com>
2025-12-18 09:57:27 +0100

    Co-authored-by: Peter Francois <Peter-Francois@users.noreply.github.com>

---
+++ src/components/ui/ErrorPageContent.tsx
@@ -0,0 +1,24 @@
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import type { ReactNode } from "react";
+import { Link } from "react-router";
+
+interface PropsInterface {
+  children: ReactNode;
+}
+
+const ErrorPageContent = ({ children }: PropsInterface) => {
+  return (
+    <div className="center flex-col gap-10 h-screen px-3 text-center">
+      <div className="center flex-col gap-3 text-red-600 text-xl">
+        <ExclamationTriangleIcon width={60} />
+        <p>{children}</p>
+      </div>
+
+      <Link to="/" className="text-indigo-500 underline">
+        Retouner à la connexion
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPageContent;
